2d70fbc38efae26160aaa46e278b357bce0dd6e9
emmanuelle <sheim33emma@gmail.com>
2026-07-22 08:22:18 +0300

    fixed main

--- ledsystem/mainprojectcode.java
+++ ledsystem/mainprojectcode.java
@@ -1,12 +1,14 @@
-package ledsystem;
+import ledsystem.*;
 import java.awt.Color;
 
-class Project1 {
-    public static void main(String[] args) {
-        LedController controller = new LedController(10);
-        BlinkAnimation blinkingAnimation= new BlinkAnimation(Color.RED, Color.BLUE);
-        controller.addAnimation(blinkingAnimation);
-        controller.play();
-    }
+public static void main(String[] args) {
+    LedController controller = new LedController(10);
+    sequentialAnimationGroup playlist = new sequentialAnimationGroup(
+            new SolidAnimation(Color.GREEN),
+            new BlinkAnimation(Color.RED, Color.BLUE),
+            new FadeAnimation(Color.YELLOW, Color.RED)
+    );
+    controller.addAnimation(playlist);
+    controller.play();
 
 }
