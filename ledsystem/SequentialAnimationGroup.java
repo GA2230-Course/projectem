265f2685ab6368b067e3a4a1a34dadb88ee5d880
emmanuelle <sheim33emma@gmail.com>
2026-07-23 15:10:36 +0300

    fixed

--- ledsystem/SequentialAnimationGroup.java
+++ ledsystem/SequentialAnimationGroup.java
@@ -1,12 +1,17 @@
 package ledsystem;
 
+import ledsystem.utils.StopWatch;
+
 public class SequentialAnimationGroup implements Animation {
     private final Animation[] animations;
+    private StopWatch currentTimer;
     private int currentIndex;
 
     public SequentialAnimationGroup(Animation... animations) {
         this.animations = animations;
         this.currentIndex = 0;
+        this.currentTimer = new StopWatch();
+        this.currentTimer.start();
     }
 
     @Override
@@ -16,12 +21,20 @@ public class SequentialAnimationGroup implements Animation {
         }
 
         Animation current = animations[currentIndex];
+
         current.apply(strip);
+
         if (current instanceof TimerAnimation) {
             TimerAnimation timerAnim = (TimerAnimation) current;
             if (timerAnim.isTimeUp()) {
                 currentIndex++;
+                this.currentTimer = new StopWatch();
+                this.currentTimer.start();
             }
+        } else {
+            currentIndex++;
+            this.currentTimer = new StopWatch();
+            this.currentTimer.start();
         }
     }
 }
--- ledsystem/mainprojectcode.java
+++ ledsystem/mainprojectcode.java
@@ -1,13 +1,16 @@
+package ledsystem;
+
 import ledsystem.*;
 import java.awt.Color;
-
-public static void main(String[] args) {
-    LedController controller = new LedController(10);
-            SequentialAnimationGroup a = new SequentialAnimationGroup(
-            new TimerAnimation(Color.RED,5),
-            new TimerAnimation(Color.BLUE,10)
-    );
-    controller.addAnimation(a);
-    controller.play();
-
+public class mainprojectcode{
+    public static void main(String[] args) {
+        LedController controller = new LedController(10);
+        SequentialAnimationGroup a = new SequentialAnimationGroup(
+                new TimerAnimation(Color.RED, 5),
+                new TimerAnimation(Color.BLUE, 10),
+                new TimerAnimation(Color.CYAN, 15)
+        );
+        controller.addAnimation(a);
+        controller.play();
+    }
 }
