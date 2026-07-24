41e3cfeb7ef50329ad34c74226a87ce5ab003db3
emmanuelle <sheim33emma@gmail.com>
2026-07-24 17:57:10 +0300

    fixed

--- ledsystem/MainProjectCode.java
+++ ledsystem/MainProjectCode.java
@@ -11,7 +11,7 @@ public class MainProjectCode {
 
         Animation redSolid = new TimerAnimation(new SolidAnimation(Color.RED), 3.0);
         Animation Blinkanimation= new TimerAnimation(new BlinkAnimation(Color.BLUE, Color.CYAN), 6.0);
-        Animation Fadeanimationcode = new TimerAnimation(new FadeAnimation(Color.MAGENTA, Color.GREEN), 4.0);
+        Animation Fadeanimationcode = new TimerAnimation(new FadeAnimation(Color.GRAY, Color.GREEN), 8.0);
 
         SequentialAnimationGroup masterSequence = new SequentialAnimationGroup(
                 redSolid,
